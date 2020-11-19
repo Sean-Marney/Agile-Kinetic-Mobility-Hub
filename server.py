5905c2aaf5eeec1c7d156ee28b88e1682566abe4
William Eddy <eddyw@cardiff.ac.uk>
2020-11-19 15:43:48 +0000

    Template server and index page added

---
+++ server.py
@@ -0,0 +1,10 @@
+import os
+import json
+from flask import Flask, redirect, request,render_template, jsonify
+
+app = Flask(__name__)
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+if __name__ == "__main__":
+    app.run(debug=True)
