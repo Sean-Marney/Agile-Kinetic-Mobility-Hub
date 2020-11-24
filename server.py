17e6cfc7aca8d2f1f6db92ac2965c8c280538406
Sean Marney <marneys@cardiff.ac.uk>
2020-11-24 13:19:11 +0000

    Created routes for index page and contact page

--- server.py
+++ server.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
+@app.route("/")
+def index():
+    return redirect("/static/index.html", code=302)
+
+@app.route("/contact", methods=['POST'])
+def contact():
+    return render_template("contact.html")
+    
+
 if __name__ == "__main__":
     app.run(debug=True)
