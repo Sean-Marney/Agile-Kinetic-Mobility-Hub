511261c2d770408e1be721a0b992c079d49b69c1
Sean Marney <marneys@cardiff.ac.uk>
2020-12-14 17:42:44 +0000

    Edited contact route in server.py so that the form from contact.html can be submitted to the server

--- server.py
+++ server.py
@@ -30,8 +30,12 @@ def index():
         return render_template("index.html", code=302, benData = getBenefit())
 
 #renders contact page
-@app.route("/contact")
+@app.route("/contact", methods=['GET, POST'])
 def contact():
+    form = ContactForm()
+    if form.is_submitted():
+        result = request.form
+        return render_template('contact.html', result=result)
     return render_template("contact.html")
 
 #renders blog posts page
