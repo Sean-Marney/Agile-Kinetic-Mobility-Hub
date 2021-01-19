a0ced229c9c000a0c3ddd2fafe54b2fad87ea263
Sean Marney <>
2021-01-19 14:20:01 +0000

    Created database and database table for contact form

---
+++ SQLDatabase.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS 'Contact' (
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "forename" TEXT,
+  "surname" TEXT,
+  "job_title" TEXT,
+  "company_title" TEXT,
+  "company_desc" TEXT,
+  "work_email" TEXT,
+  "phone_No." TEXT,
+  "message" TEXT
+)
